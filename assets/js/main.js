dd675fdbe951fd002277ed11e67837fcf1b6f15f
Cristobal Andres Manzano Martinez <zano@live.cl>
2022-02-28 23:20:49 -0300

    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: false,
+        loop: false
+      }
+    }
+  })
+})
+/* TOOLTIP */
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+})
+/* ALERTA BOTON */
+$("#enviarCorreo").click(function () {
+  alert("El correo fue enviado correctamente");
+});
+$("#agregarFavorito").click(function () {
+  alert("Se ha agregado la receta a tus favoritos");
+});
+/*CAMBIO DE COLOR */
+$(".ingredients").on('dblclick', function () {
+  $(this).css('color', 'red');
+});
+$(".making").on('dblclick', function () {
+  $(this).css('color', 'red');
+});
+/* TOGGLE INFORMACION */
+$('.card-title').on('click', function () {
+  $('.card-text').toggle();
+});
